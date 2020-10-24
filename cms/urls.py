eebabbb8d43630caba3aff50339ced543440afe7
Calum <calumstah@gmail.com>
2020-10-24 18:38:17 +0800

    style: add plain templates for: contact, faq, privacy policy, support, and tnc

--- cms/urls.py
+++ cms/urls.py
@@ -7,10 +7,10 @@ app_name = 'cms'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     # path('redirect/', views.RedirectView.as_view(), name='redirect'),
-    # path('faq/', views.FAQView.as_view(), name='faq'),
-    # path('T&C/', views.TermsAndConditionView.as_view(), name='t&c'),
-    # path('privacy-policy/', views.PrivacyPolicyView.as_view(), name='privacy-policy'),
-    # path('contact/', views.ContactView.as_view(), name='contact'),
-    # path('support/', views.SupportView.as_view(), name='support'),
+    path('faq/', views.FAQView.as_view(), name='faq'),
+    path('tnc/', views.TermsAndConditionView.as_view(), name='t&c'),
+    path('privacy-policy/', views.PrivacyPolicyView.as_view(), name='privacy-policy'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('support/', views.SupportView.as_view(), name='support'),
     
 ]
